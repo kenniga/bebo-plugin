69a23aef8a322b613e5505548ef476717a9a1a2c
Kenni Gandira Alamsyah <kennialamsyah@Kenni-Alamsyah.local>
2019-08-31 23:29:53 +0700

    fix

--- includes/widgets/new-widget.php
+++ includes/widgets/new-widget.php
@@ -1,18 +1,11 @@
 <?php
+class Dahz_Framework_Widget_Product_Category extends WP_Widget {
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
-
-if( class_exists( 'WC_Widget' ) ){
-
-	class Dahz_Framework_Widget_Product_Category extends WC_Widget {
-
-		/**
-		 * Constructor.
-		 */
-		public function __construct() {
-			$this->widget_cssclass    = 'woocommerce de-widget widget_product_category';
+	/**
+	 * Setup widget: Name, base ID
+	 */
+	public function __construct() {
+		$this->widget_cssclass    = 'woocommerce de-widget widget_product_category';
 			$this->widget_description = esc_html__( 'Display a list of product categories from your store.', 'pabu' );
 			$this->widget_id          = 'dahz_woocommerce_product_category';
 			$this->widget_name        = esc_html__( 'Dahz - Product Category', 'pabu' );
@@ -39,96 +32,88 @@ if( class_exists( 'WC_Widget' ) ){
 			);
 
 			parent::__construct();
-		}
-
-		/**
-		 * Output widget.
-		 *
-		 * @see WP_Widget
-		 *
-		 * @param array $args
-		 * @param array $instance
-		 */
-		 public function widget( $args, $instance ) {
-
-			if ( $this->get_cached_widget( $args ) ) {
-				return;
-			}
-
-			ob_start();
-
-			$category_ids = ! empty( $instance['category_ids'] ) ? $instance['category_ids'] : $this->settings['category_ids']['std'];
-			$display_as   = ! empty( $instance['display_as'] ) ? $instance['display_as'] : $this->settings['display_as']['std'];
-
-			$category_ids = explode( ',', $category_ids );
-
-			$this->widget_start( $args, $instance );
-
-			$category_html = '';
-
-			foreach( $category_ids as $key => $category_id ) {
-
-				$category = get_term_by( 'slug', $category_id, 'product_cat' );
-
-				if( $category ){
-
-					$category_url = get_term_link( $category->term_id );
-
-					$image_id  = get_woocommerce_term_meta( $category->term_id, 'thumbnail_id', true );
-
-					$category_image = wp_get_attachment_image( $image_id, 'medium' );
-
-					$category_html .= sprintf(
-						'
-						<li class="dont know">
-							<a href="%1$s">
-								%2$s
-								<span>
-									<h4>%3$s</h4>
-								</span>
-							</a>
-						</li>
-						',
-						esc_url( $category_url ),
-						$category_image ,
-						esc_html( $category->name )
-					);
-
-				}
-
-			}
-
-			echo sprintf(
-				'
-				<ul data-display="%1$s">
-					%2$s
-				</ul>
-				',
-				esc_attr( $display_as ),
-				$category_html
-
-			);
-
-			$this->widget_end( $args );
-
-			$content = ob_get_clean();
-
-			echo( $content );
-
-			$this->cache_widget( $args, $content );
-		}
 	}
 
-}
+	/**
+	 * Show widget
+	 */
 
+	public function widget( $args, $instance ) {
+
+		if ( $this->get_cached_widget( $args ) ) {
+      return;
+    }
+
+    ob_start();
+
+    $category_ids = ! empty( $instance['category_ids'] ) ? $instance['category_ids'] : $this->settings['category_ids']['std'];
+    $display_as   = ! empty( $instance['display_as'] ) ? $instance['display_as'] : $this->settings['display_as']['std'];
+
+    $category_ids = explode( ',', $category_ids );
+
+    $this->widget_start( $args, $instance );
+
+    $category_html = '';
+
+    foreach( $category_ids as $key => $category_id ) {
+
+      $category = get_term_by( 'slug', $category_id, 'product_cat' );
+
+      if( $category ){
+
+        $category_url = get_term_link( $category->term_id );
+
+        $image_id  = get_woocommerce_term_meta( $category->term_id, 'thumbnail_id', true );
+
+        $category_image = wp_get_attachment_image( $image_id, 'medium' );
+
+        $category_html .= sprintf(
+          '
+          <li class="dont know">
+            <a href="%1$s">
+              %2$s
+              <span>
+                <h4>%3$s</h4>
+              </span>
+            </a>
+          </li>
+          ',
+          esc_url( $category_url ),
+          $category_image ,
+          esc_html( $category->name )
+        );
+
+      }
+
+    }
+
+    echo sprintf(
+      '
+      <ul data-display="%1$s">
+        %2$s
+      </ul>
+      ',
+      esc_attr( $display_as ),
+      $category_html
+
+    );
+
+    $this->widget_end( $args );
+
+    $content = ob_get_clean();
+
+    echo( $content );
+
+    $this->cache_widget( $args, $content );
+	}
+}
 
 /*
  * Create widget item
  */
-
-add_action( 'widgets_init', 'sf_category_widget' );
-function sf_category_widget() {
+add_action( 'widgets_init', 'dahz_woocommerce_product_category' );
+function dahz_woocommerce_product_category() {
 	if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-		register_widget('Dahz_Framework_Widget_Product_Category');
+		register_widget('dahz_woocommerce_product_category');
 	}
 }
